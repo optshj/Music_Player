19e5ad22d40d0bf978e343f8dc63e833a3ed47dc
optshj <optshj@gmail.com>
2023-09-01 14:01:25 +0000

    React_init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import logo from './component/logo.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+			<logo/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../css/logo.module.css';
+
+function logo(){
+	
+	return(
+		<div>
+			<h1 className={styles.biglogo}>Music Player</h1>
+		</div>
+	)
+}
+			
+export default logo;
